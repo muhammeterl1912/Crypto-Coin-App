a47c6f0834f72e0d32589ff5678379341ad5316f
Muhammet Erol <muhammederol971@gmail.com>
2024-03-14 03:33:42 +0100

    started to code main.js

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 import "./scss/style.scss"
+import { getCoins } from "./src/getCoins"
+// Selector
+const form = document.querySelector("form")
 
+// Event
+form.addEventListener("submit",(event)=>{
+event.preventDefault()
+event.target.reset() 
+} )
 
-document.querySelector('#app').innerHTML = `
-
-`
+const getCoibsData = () =>{
+  const input = document.querySelector("form input").value
+  if (!input.trim()) {
+    alert("Input area can not be empty.")
+  } else {
+    getCoins()
+  }
+}
